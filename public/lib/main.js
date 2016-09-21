703a543aeba456e40eb8d37f35eea503aa6cde60
Sean H <e@shass.co>
2016-09-21 15:44:28 +1000

    Addition of React-Router.  E.g. needs some work

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 
 const DOM_APP_EL_ID = 'app';
 
+class HomePage extends React.Component {
+  render() {
+    return <p>Home page</p>
+  }
+}
+
+class AboutPage extends React.Component {
+  render() {
+    return <p>About page</p>
+  }
+}
 ReactDOM.render((
-  <div>From the app</div>
+  <Router history={browserHistory}>
+    <Route path="/">
+      <IndexRoute component={HomePage}/>
+      <Route path="page-two" component={AboutPage}/>
+    </Route>
+  </Router>
 ), document.getElementById(DOM_APP_EL_ID));
